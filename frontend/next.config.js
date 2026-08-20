6f8e01836b7063a3682b3fd34b17d15324a42ca8
H1B0B0 <hibosmurf@gmail.com>
2026-08-20 13:42:40 +0200

    chore: configure next.js for static export (Tauri preparation)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
